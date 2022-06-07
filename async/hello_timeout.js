a8c75b75185c71cbd6ae450d85b9c67f5e421244
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-07 21:38:35 +0000

    Initial commit

---
+++ async/hello_timeout.js
@@ -0,0 +1,15 @@
+// setTimeout (() => {
+//   console.log("4311o th3r3 w0r1d");
+// }, "3000")
+
+setTimeout (() => {
+  console.log("4311o");
+},)
+
+setTimeout (() => {
+  console.log("th3r3");
+}, "1000")
+
+setTimeout (() => {
+  console.log("w0r1d");
+}, "2000")
